70d757ca46b3d4d85b5d1a4d41f1ab8fdc73972a
heyglen <glencharmon@gmail.com>
2016-02-17 15:22:22 +0100

    Network mask type validation and conversion

--- netobj/nutility.py
+++ netobj/nutility.py
@@ -7,17 +7,56 @@ class NUtility(object):
     wildcards = [ipaddress.IPv4Interface(unicode('0.0.0.0/{0}'.format(x))).network.hostmask.exploded for x in range(0, 33)]
     netmasks = [ipaddress.IPv4Interface(unicode('255.255.255.255/{0}'.format(x))).network.network_address.exploded for x in range(0, 33)]
 
+    class validate(object):
+        @staticmethod
+        def netmask(netmask):
+            return netmask in NUtility.netmasks
+
+        @staticmethod
+        def wildcard(wildcard):
+            return wildcard in NUtility.wildcards
+
+        @staticmethod
+        def prefix(prefix):
+            return prefix in range(0, 33)
+
     class convert(object):
+
+        class prefix(object):
+            @staticmethod
+            def netmask(prefix):
+                if not NUtility.validate.prefix(prefix):
+                    raise ValueError(u'Invalid prefix length \'{}\''.format(prefix))
+                return NUtility.netmasks[prefix]
+
+            @staticmethod
+            def wildcard(prefix):
+                if not NUtility.validate.prefix(prefix):
+                    raise ValueError(u'Invalid prefix length \'{}\''.format(prefix))
+                return NUtility.wildcards[prefix]
+
         class netmask(object):
             @staticmethod
             def wildcard(netmask):
-                if netmask not in NUtility.netmasks:
-                    raise ValueError(u'Invalid netmask {}'.format(netmask))
+                if not NUtility.validate.netmask(netmask):
+                    raise ValueError(u'Invalid netmask \'{}\''.format(netmask))
                 return NUtility.wildcards[NUtility.netmasks.index(netmask)]
+
+            @staticmethod
+            def prefix(netmask):
+                if not NUtility.validate.netmask(netmask):
+                    raise ValueError(u'Invalid netmask \'{}\''.format(netmask))
+                return NUtility.netmasks.index(netmask)
 
         class wildcard(object):
             @staticmethod
             def netmask(wildcard):
-                if wildcard not in NUtility.wildcards:
-                    raise ValueError(u'Invalid wildcard {}'.format(wildcard))
+                if not NUtility.validate.wildcard(wildcard):
+                    raise ValueError(u'Invalid wildcard \'{}\''.format(wildcard))
                 return NUtility.netmasks[NUtility.wildcards.index(wildcard)]
+
+            @staticmethod
+            def prefix(wildcard):
+                if not NUtility.validate.wildcard(wildcard):
+                    raise ValueError(u'Invalid wildcard \'{}\''.format(wildcard))
+                return NUtility.wildcards.index(wildcard)
--- test/test_net_tool.py
+++ test/test_net_tool.py
@@ -2,7 +2,7 @@
 
 import ipaddress
 
-from nose.tools import assert_equals, assert_raises
+from nose.tools import assert_equals, assert_raises, assert_true, assert_false
 
 from netobj.nutility import NUtility
 
@@ -13,15 +13,62 @@ class TestNetTool(object):
         self.netmasks = [ipaddress.IPv4Interface(unicode('255.255.255.255/{0}'.format(x))).network.network_address.exploded for x in range(0, 33)]
         self.wildcards = [ipaddress.IPv4Interface(unicode('0.0.0.0/{0}'.format(x))).network.hostmask.exploded for x in range(0, 33)]
 
-    def test_wildcard_to_netmask_conversion_invalid_input(self):
-        assert_raises(ValueError, NUtility.convert.netmask.wildcard, 1)
+    def test_netmask_validation(self):
+        for netmask in self.netmasks:
+            assert_true(NUtility.validate.netmask(netmask))
+        assert_false(NUtility.validate.netmask('netmask'))
+        assert_false(NUtility.validate.netmask(33))
 
-    def test_wildcard_to_netmask_conversion(self):
-        assert_equals(NUtility.convert.netmask.wildcard('255.255.0.0'), '0.0.255.255')
+    def test_wildcard_validation(self):
+        for wildcard in self.wildcards:
+            assert_true(NUtility.validate.wildcard(wildcard))
+        assert_false(NUtility.validate.wildcard('wildcard'))
+        assert_false(NUtility.validate.wildcard(33))
+
+    def test_prefix_validation(self):
+        for wildcard in self.wildcards:
+            assert_true(NUtility.validate.wildcard(wildcard))
+        assert_false(NUtility.validate.wildcard('wildcard'))
+        assert_false(NUtility.validate.wildcard(33))
+
+    def test_netmask_to_wildcard_conversion(self):
         for index, netmask in enumerate(self.netmasks):
             assert_equals(NUtility.convert.netmask.wildcard(netmask), self.wildcards[index])
 
-    def test_netmask_to_wildcard_conversion(self):
-        assert_equals(NUtility.convert.wildcard.netmask('0.0.255.255'), '255.255.0.0')
+    def test_netmask_to_prefix_conversion(self):
+        for index, netmask in enumerate(self.netmasks):
+            assert_equals(NUtility.convert.netmask.prefix(netmask), index)
+
+    def test_wildcard_to_netmask_conversion(self):
         for index, wildcard in enumerate(self.wildcards):
             assert_equals(NUtility.convert.wildcard.netmask(wildcard), self.netmasks[index])
+
+    def test_wildcard_to_prefix_conversion(self):
+        for index, wildcard in enumerate(self.wildcards):
+            assert_equals(NUtility.convert.wildcard.prefix(wildcard), index)
+
+    def test_prefix_to_netmask_conversion(self):
+        for prefix in range(0, 33):
+            assert_equals(NUtility.convert.prefix.netmask(prefix), self.netmasks[prefix])
+
+    def test_prefix_to_wildcard_conversion(self):
+        for prefix in range(0, 33):
+            assert_equals(NUtility.convert.prefix.wildcard(prefix), self.wildcards[prefix])
+
+    def test_netmask_to_wildcard_conversion_invalid_input(self):
+        assert_raises(ValueError, NUtility.convert.netmask.wildcard, 1)
+
+    def test_netmask_to_prefix_conversion_invalid_input(self):
+        assert_raises(ValueError, NUtility.convert.netmask.prefix, 1)
+
+    def test_wildcard_to_netmask_conversion_invalid_input(self):
+        assert_raises(ValueError, NUtility.convert.wildcard.netmask, 1)
+
+    def test_wildcard_to_prefix_conversion_invalid_input(self):
+        assert_raises(ValueError, NUtility.convert.wildcard.prefix, 1)
+
+    def test_prefix_to_netmask_conversion_invalid_input(self):
+        assert_raises(ValueError, NUtility.convert.prefix.netmask, 'invalid')
+
+    def test_prefix_to_wildcard_conversion_invalid_input(self):
+        assert_raises(ValueError, NUtility.convert.prefix.wildcard, 'invalid')
