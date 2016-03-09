660f389ad49ad9c99e7772bb262f2fcecae2ec1e
Glen Harmon <glencharmon@gmail.com>
2016-03-09 22:32:07 +0100

    Feature: Cisco ASA - ACE expanded syntax

--- nettool/vendor/cisco/asa/ace.py
+++ nettool/vendor/cisco/asa/ace.py
@@ -4,41 +4,80 @@ from ipaddress import IPv4Network
 from nettool.ace import Ace as GenericAce
 
 
-class Ace(GenericAce):
+class AsaAddress(object):
+    valid_protocols = ('ip', 'tcp', 'udp')
 
     @classmethod
-    def _format_protocol(cls, source_transport, destination_transport):
-        protocol = 'ip'
-        if destination_transport is not None:
-            protocol = destination_transport[0].type.lower()
-            if protocol not in ('tcp', 'udp'):
-                if source_transport is not None:
-                    protocol = source_transport[0].type.lower()
+    def protocol(cls, source_transport, destination_transport):
+        if source_transport is None and destination_transport is None:
+            protocol = 'ip'
+        else:
+            if destination_transport is not None:
+                protocol = destination_transport.type.lower()
+                if protocol not in cls.valid_protocols:
+                    if source_transport is not None:
+                        protocol = source_transport.type.lower()
+        if protocol == 'tcp/udp':
+            protocol = 'ip'
+        if protocol not in cls.valid_protocols:
+            message = 'Invalid protocol {}. Must be: {}'
+            message = message.format(protocol, ', '.join(cls.valid_protocols))
+            raise ValueError(message)
         return protocol
 
     @classmethod
-    def _format_protocol_name(cls, transport):
+    def protocol_group(cls, transport):
         protocol = 'ip'
         if transport is not None:
             if transport.name is not None:
                 protocol = 'object-group {}'.format(transport.name)
             else:
-                protocol = cls._format_protocol(transport.source, transport.destination)
+                protocol = cls.protocol(transport.source[0], transport.destination[0])
         return protocol
 
     @classmethod
-    def _format_transport(cls, transport_group):
+    def transport(cls, transport):
         port = ''
-        if transport_group is not None:
-            if transport_group != transport_group.__class__():
-                port = transport_group._port_string()
+        if transport is not None:
+            if transport != transport.__class__():
+                port = transport._port_string()
+                port = port.replace('-', ' ')
+                if len(port.split()) == 2:
+                    port = 'range {}'.format(port)
+                else:
+                    port = 'eq {}'.format(port)
         return port
 
     @classmethod
-    def _format_network(cls, network):
+    def transport_group(cls, transport):
+        if transport.name is not None:
+            transport = 'object-group {}'.format(transport.name)
+        else:
+            transport = cls.transport(transport[0])
+        return transport
+
+    @classmethod
+    def network(cls, network):
         if isinstance(network, IPv4Network):
             network = '{} {}'.format(str(network.network_address), str(network.netmask))
         return network
+
+    @classmethod
+    def network_group(cls, network):
+        if network.name is None:
+            network = cls.network(network[0])
+        else:
+            network = 'object-group {}'.format(network.name)
+        return network
+
+
+class Ace(GenericAce):
+
+    def show_run(self):
+        return self._ace_print(expand=False)
+
+    def show(self):
+        return self._ace_print(expand=True)
 
     def _is_grouped(self):
         """ Does the ACE consist of groupings? """
@@ -51,34 +90,22 @@ class Ace(GenericAce):
         names.append(self.network.destination.name)
         return any(names)
 
-    def _grouped_ace(self, prefix, suffix):
+    def _collapsed_print(self, prefix, suffix):
         output = list()
         line = list()
 
         line.append(prefix)
-        line.append(self._format_protocol_name(self.transport))
+        line.append(AsaAddress.protocol_group(self.transport))
 
         for side in ('source', 'destination'):
-            if getattr(self.network, side).name is not None:
-                line.append('object-group {}'.format(getattr(self.network, side).name))
-            else:
-                line.append(self._format_network(getattr(self.network, side)[0]))
+            line.append(AsaAddress.network_group(getattr(self.network, side)))
             if self.transport is not None and self.transport.name is None:
-                if getattr(self.transport, side).name is not None:
-                    line.append('object-group {}'.format(getattr(self.transport, side).name))
-                else:
-                    line.append(self._format_transport(getattr(self.transport, side)[0]))
+                line.append(AsaAddress.transport_group(getattr(self.transport, side)))
         line.append(suffix)
         output.append(' '.join(line).replace('  ', ' ').strip())
         return output
 
-    def show_run(self):
-        return self._asa_print(expand=False)
-
-    def show(self):
-        return self._asa_print(expand=True)
-
-    def _asa_print(self, expand=False):
+    def _ace_print(self, expand=False):
         permission = 'permit'
         if not self.permit:
             permission = 'deny'
@@ -89,22 +116,19 @@ class Ace(GenericAce):
             suffix = self.logging.name
         output = list()
 
-        if self._is_grouped():
-            output = self._grouped_ace(prefix, suffix)
+        if not expand and self._is_grouped():
+            output = self._collapsed_print(prefix, suffix)
 
-        if self.transport is None:
-            prefix = '{} ip'.format(prefix)
-        if expand:
+        if expand or (not expand and not self._is_grouped()):
             for source_network, destination_network in self._iter_networks():
                 for source_transport, destination_transport in self._iter_transport():
                     line = list()
                     line.append(prefix)
-                    protocol = self._format_protocol(source_transport, destination_transport)
-                    line.append(protocol)
-                    line.append(self._format_network(source_network))
-                    line.append(self._format_transport(source_transport, name=False))
-                    line.append(self._format_network(destination_network))
-                    line.append(self._format_transport(destination_transport, name=False))
+                    line.append(AsaAddress.protocol(source_transport, destination_transport))
+                    line.append(AsaAddress.network(source_network))
+                    line.append(AsaAddress.transport(source_transport))
+                    line.append(AsaAddress.network(destination_network))
+                    line.append(AsaAddress.transport(destination_transport))
                     line.append(suffix)
                     output.append(' '.join(line).replace('  ', ' ').strip())
         return '\n'.join(output)
--- test/vendor/cisco/asa/test_vendor_cisco_asa_ace.py
+++ test/vendor/cisco/asa/test_vendor_cisco_asa_ace.py
@@ -28,22 +28,12 @@ class TestVendorCiscoAsaAce(object):
         ace.transport.destination.name = None
         assert_false(ace._is_grouped())
 
-    def test_show_run_group_network(self):
-        ace = Ace(network='1.2.3.0/24 4.5.6.0/24')
-        ace.network.source.name = 'source'
-        ace.network.destination.name = 'destination'
-        expected = 'permit ip object-group source object-group destination'
+    def test_show_run_default(self):
+        ace = Ace()
+        expected = 'permit ip 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0'
         assert_equals(ace.show_run(), expected)
 
-    def test_show_run_group_transport(self):
-        ace = Ace(transport=TransportLayer(name='service'))
-        expected = 'permit object-group service 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0'
-        assert_equals(ace.show_run(), expected)
-        ace.transport.destination.add('tcp 22')
-        ace.transport.destination.add('udp 161')
-        assert_equals(ace.show_run(), expected)
-
-    def test_show_run_group_protocol(self):
+    def test_show_run_protocol(self):
         ace = Ace(transport='tcp 22')
         ace.transport.destination.name = 'ssh'
         expected = 'permit tcp 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 object-group ssh'
@@ -56,3 +46,57 @@ class TestVendorCiscoAsaAce(object):
         ace.transport.destination.add('udp 1-65535')
         expected = 'permit udp 0.0.0.0 0.0.0.0 object-group snmp-trap 0.0.0.0 0.0.0.0'
         assert_equals(ace.show_run(), expected)
+
+    def test_show_run_network(self):
+        ace = Ace(network='1.2.3.0/24 4.5.6.0/24')
+        ace.network.source.name = 'source'
+        ace.network.destination.name = 'destination'
+        expected = 'permit ip object-group source object-group destination'
+        assert_equals(ace.show_run(), expected)
+
+    def test_show_run_transport(self):
+        ace = Ace(transport=TransportLayer(name='service'))
+        expected = 'permit object-group service 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0'
+        assert_equals(ace.show_run(), expected)
+        ace.transport.destination.add('tcp 22')
+        ace.transport.destination.add('udp 161')
+        assert_equals(ace.show_run(), expected)
+
+    def test_show_default(self):
+        ace = Ace()
+        expected = 'permit ip 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0'
+        assert_equals(ace.show(), expected)
+
+    def test_show_protocol(self):
+        ace = Ace(transport='tcp 22')
+        ace.transport.destination.name = 'ssh'
+        expected = 'permit tcp 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 eq 22'
+        assert_equals(ace.show(), expected)
+        ace.transport.destination.name = None
+        ace.transport.source.remove('tcp 1-65535')
+        ace.transport.destination.remove('tcp 22')
+        ace.transport.source.name = 'snmp-trap'
+        ace.transport.source.add('udp 162')
+        ace.transport.destination.add('udp 1-65535')
+        expected = 'permit udp 0.0.0.0 0.0.0.0 eq 162 0.0.0.0 0.0.0.0'
+        assert_equals(ace.show(), expected)
+
+    def test_show_network(self):
+        ace = Ace(network='1.2.3.0/24 4.5.6.0/24')
+        ace.network.source.name = 'source'
+        ace.network.destination.name = 'destination'
+        expected = 'permit ip 1.2.3.0 255.255.255.0 4.5.6.0 255.255.255.0'
+        assert_equals(ace.show(), expected)
+
+    def test_show_transport(self):
+        ace = Ace(transport=TransportLayer(name='service'))
+        expected = 'permit ip 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0'
+        assert_equals(ace.show(), expected)
+        ace.transport.source.add('tcp 1-65535')
+        ace.transport.destination.add('tcp 22')
+        expected = 'permit tcp 0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 eq 22'
+        assert_equals(ace.show(), expected)
+        ace.transport.source.remove('tcp 1-65535')
+        ace.transport.source.add('tcp 1-22')
+        expected = 'permit tcp 0.0.0.0 0.0.0.0 range 1 22 0.0.0.0 0.0.0.0 eq 22'
+        assert_equals(ace.show(), expected)
