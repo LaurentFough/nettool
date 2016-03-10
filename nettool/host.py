4666b2358c64c236170fe6a099424241a8a7b464
Glen Harmon <glencharmon@gmail.com>
2016-03-10 21:04:25 +0100

    Refactor: Reduced cyclomatic complexity

--- nettool/host.py
+++ nettool/host.py
@@ -2,7 +2,7 @@
 
 from nettool.hostname import Hostname
 from nettool.hostname_list import HostnameList
-from nettool.nettest import NetTest as nu
+from nettool.utilities import raise_type_exception
 
 
 class Host(HostnameList):
@@ -15,51 +15,47 @@ class Host(HostnameList):
     def __eq__(self, value):
         return self.__contains__(value)
 
+    def _update_hostname_attributes(self, hostname):
+        """ Update exisiting hostnames without an attribute with the new hosts' attribute """
+        for attribute in ('domain', 'ip'):
+            for self_host in self._host_entries:
+                if hostname.name == self_host.name:
+                    if not getattr(self_host, attribute):
+                        setattr(self_host, attribute, getattr(hostname, attribute))
+
+    def _add_hostname_new_ip(self, hostname):
+        for entry in self._host_entries:
+            if entry.fqdn == hostname.fqdn and entry.ip != hostname.ip:
+                self._append(hostname)
+                return
+
+    def _add_ip_new_hostname(self, hostname):
+        for entry in self._host_entries:
+            if entry.ip == hostname.ip and entry.name != hostname.name:
+                self._append(hostname)
+                return
+
     def add(self, value, ip=None):
         """ Merges a value with existing host entry values """
         if isinstance(value, basestring):
             value = Hostname(value, ip=ip)
 
         if not isinstance(value, Hostname):
-            raise TypeError('Can only add Hostname types')
+            raise_type_exception(value, (Hostname, ), 'add')
 
         if value not in self._host_entries:
             raise ValueError('Host {} does not belong to {}'.format(value, self))
-        # If existing Hostname matches the new one on all attributes, do nothing
-        for entry in self._host_entries:
-            if value.fqdn == entry.fqdn and entry.ip == value.ip:
-                return True
 
-        # If existing Hostname name matches but has no domain, add the domain
-        #  to the existing Hostname
-        for entry in self._host_entries:
-            if entry.name == value.name and not entry.domain:
-                entry.domain = value.domain
-                break
+        self._update_hostname_attributes(value)
 
-        # If existing Hostname name matches but has no IP, add the IP to the existing Hostname
-        if nu.validate.ip(value.ip):
-            for entry in self._host_entries:
-                if value.name == entry.name and not entry.ip:
-                    entry.ip = value.ip
-
-        # If existing Hostname name matches but the ip is different, add the new entry to the list
-        existing_hostname = False
         for entry in self._host_entries:
-            if entry.fqdn == value.fqdn and entry.ip != value.ip:
-                existing_hostname = True
-                break
-        if existing_hostname:
-            self._append(value)
+            if value.fqdn == entry.fqdn:
+                if entry.ip == value.ip:
+                    # Full match found. Do nothing
+                    return
 
-        # New hostname on an existing IP
-        existing_ip = False
-        for entry in self._host_entries:
-            if entry.ip == value.ip and entry.name != value.name:
-                existing_ip = True
-                break
-        if existing_ip:
-            self._append(value)
+        self._add_hostname_new_ip(value)
+        self._add_ip_new_hostname(value)
 
     @property
     def display_hostname(self):
